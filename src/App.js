aed7c216481eefa19031b8585a65da566438c607
Yuta Ushijima <register@yuta-u.com>
2019-01-05 17:29:01 +0900

    mapStateToPropsを定義

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// 個別のコンポーネントからStoreの情報にアクセスするため、
+// connect関数をインポートする
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
@@ -10,21 +13,27 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Welcome to React Redux Todo list</h1>
         </header>
+          <ul>
+              {this.props.todos.map(todo => {
+                  return (
+                      <li key={todo}>{todo}</li>
+                  );
+              })}
+          </ul>
       </div>
     );
   }
 }
 
-export default App;
+// mapStateToProps...Storeの情報をpropsに反映させる、1つのオブジェクトを返す関数
+// stateを引数(中身はstate.todosとして保存されているstoreの値)にしている。
+const mapStateToProps = state => {
+    return {
+        todos: state.todos.list
+    }
+};
+
+// Appを引数にして、connect経由でApp呼び出す。
+export default connect(mapStateToProps)(App);
