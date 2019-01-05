293a485c3cd8e455bc341ff42cc46dfc27eb9aa2
Yuta Ushijima <register@yuta-u.com>
2019-01-05 17:28:54 +0900

    Providerを定義

--- src/index.js
+++ src/index.js
@@ -5,8 +5,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// 動作確認として、React側からStoreの中のデータを利用するため、Providerをインポート
+import { Provider } from 'react-redux';
+import store from "./store";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    // store={store}という属性値を渡すことで、Storeの持つ情報(todosの中身)にアクセスできる
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
